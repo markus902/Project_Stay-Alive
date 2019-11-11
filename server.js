cbc30a7bf0d6a7a546e4f384ccf6d7297a9bf9cd
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-11-11 11:25:16 -0500

    merged with Markus branch

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var mySQL = require("sequelize");
+// var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Routes
+// app.use("/", require("./routes/routes"));
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
