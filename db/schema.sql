ca65f32d0357f70eb3ede5fa1289c698ec0c715f
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-11-11 11:19:33 -0500

    Added to Models Folder for sequelize.  Borrowed some data from Valhalla and there are commented out lines from that project.

--- db/schema.sql
+++ db/schema.sql
@@ -6,11 +6,11 @@ USE stay_alive_db;
 CREATE TABLE `User` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(30) NOT NULL DEFAULT '',
-  `username` varchar(30) NOT NULL DEFAULT '',
+  `userName` varchar(30) NOT NULL DEFAULT '',
   `password` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
-  `first_name` varchar(30) NOT NULL DEFAULT '',
-  `last_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `firstName` varchar(30) NOT NULL DEFAULT '',
+  `lastName` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `characterID` int(11) NOT NULL,
-  `last_login` datetime NOT NULL,
+  `lastLogin` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
