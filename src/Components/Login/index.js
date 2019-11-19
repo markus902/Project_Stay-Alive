921c91456d88b61399bba86d820e3e963a9ee70c
mhayes428 <fikes428@gmail.com>
2019-11-18 20:48:50 -0500

    login/signup

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,18 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
+// import { Face, Fingerprint } from '@material-ui/icons'
 
-
-class Login extends Component {
-  state={
-
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit * 2,
+  },
+  padding: {
+    padding: theme.spacing.unit
   }
+});
 
-  render(){
-    return(
-      <div>
-        <h1>Login</h1>
-      </div>
-    )
+class Login extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.padding}>
+        <div className={classes.margin}>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              {/* <Face /> */}
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField id="username" label="Username" type="email" fullWidth autoFocus required />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              {/* <Fingerprint /> */}
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField id="username" label="Password" type="password" fullWidth required />
+            </Grid>
+          </Grid>
+          <Grid container alignItems="center" justify="space-between">
+            {/* <Grid item>
+                            <FormControlLabel control={
+                                <Checkbox
+                                    color="primary"
+                                />
+                            } label="Remember me" />
+                        </Grid> */}
+            <Grid item>
+              {/* <Button disableFocusRipple disableRipple style={{ textTransform: "none" }} variant="text" color="primary">Forgot password ?</Button> */}
+            </Grid>
+          </Grid>
+          <Grid container justify="center" style={{ marginTop: '10px' }}>
+            <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Login</Button>
+          </Grid>
+          <Grid container justify="center" style={{ marginTop: '10px' }}>
+            <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Register</Button>
+          </Grid>
+        </div>
+      </Paper>
+    );
   }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
--- src/Components/Signup/index.js
+++ src/Components/Signup/index.js
@@ -1,18 +1,81 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
+// import { Face, Fingerprint } from '@material-ui/icons'
 
-
-class SignUp extends Component {
-  state={
-
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit * 2,
+  },
+  padding: {
+    padding: theme.spacing.unit
   }
+});
 
-  render(){
-    return(
-      <div>
-        <h1>SignUp</h1>
-      </div>
-    )
+class Login extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.padding}>
+        <div className={classes.margin}>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              {/* <Face /> */}
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField id="firstName" label="First Name" type="name" fullWidth autoFocus required />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              {/* <Fingerprint /> */}
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField id="lastName" label="Last Name" type="name" fullWidth required />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              {/* <Fingerprint /> */}
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField id="username" label="Email" type="email" fullWidth required />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              {/* <Face /> */}
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField id="userName" label="Username" type="" fullWidth autoFocus required />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              {/* <Fingerprint /> */}
+            </Grid>
+            <Grid item md={true} sm={true} xs={true}>
+              <TextField id="password" label="Password" type="password" fullWidth required />
+            </Grid>
+          </Grid>
+          <Grid container alignItems="center" justify="space-between">
+            <Grid item>
+              {/* <FormControlLabel control={
+                                <Checkbox
+                                    color="primary"
+                                />
+                            } label="Remember me" /> */}
+            </Grid>
+            <Grid item>
+              {/* <Button disableFocusRipple disableRipple style={{ textTransform: "none" }} variant="text" color="primary">Forgot password ?</Button> */}
+            </Grid>
+          </Grid>
+          <Grid container justify="center" style={{ marginTop: '10px' }}>
+            <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Register</Button>
+          </Grid>
+        </div>
+      </Paper>
+    );
   }
 }
 
-export default SignUp;
+export default withStyles(styles)(Login);
