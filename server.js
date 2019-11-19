1c316e1921bc1133aff21912dd004feef8b73423
Markus Markowich <markus902@gmx.de>
2019-11-18 19:24:04 -0500

    added middle ware and requirements for authentication

--- server.js
+++ server.js
@@ -13,8 +13,39 @@ app.use(express.urlencoded({
     extended: true,
 }));
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "public")));
+
+// Authentication Middleware
+
+if (!authConfig.domain || !authConfig.audience) {
+    throw new Error(
+        "Please make sure that auth_config.json is in place and populated"
+    );
+}
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.static(join(__dirname, "build")));
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+    }),
+
+    audience: authConfig.audience,
+    issuer: `https://${authConfig.domain}/`,
+    algorithm: ["RS256"]
+});
+
+app.get("/api/external", checkJwt, (req, res) => {
+    res.send({
+        msg: "Your access token was successfully validated!"
+    });
+});
+
 
 // Routes
 app.use("/api", require("./routes/apiRoutes"));
