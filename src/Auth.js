58d4feb9f964362ef12fbc2a4bed6dfde6c68470
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-11-30 15:23:02 -0500

    Still working on fitbit web api call

--- src/Auth.js
+++ src/Auth.js
@@ -35,11 +35,17 @@ class Auth {
   }
 
   isAuthenticated() {
-    return new Date().getTime() < this.expiresAt;
+    return new Date().getTime() < this.expires_in;
   }
 
   signIn() {
     this.auth0.authorize();
+  }
+
+  getRefreshToken() {
+    let refreshToken = this.refreshToken;
+    console.log("refreshToken: ", refreshToken);
+    return refreshToken;
   }
 
   handleAuthentication() {
@@ -56,27 +62,64 @@ class Auth {
   }
 
   setSession(authResult) {
-    console.log(authResult.idToken);
-    console.log(authResult.accessToken);
-    console.log(authResult.idTokenPayload);
-    // Also Need a refresh token that doesnt change.
-    ///// test the fitbit call
-    console.log(process.env.REACT_APP_JeremyTOKEN); // not working
-    console.log(process.env.REACT_APP_JeremyUSERID); // not working
-    console.log(process.env.REACT_APP_JeremyUSERID);
+    console.log("idToken: ", authResult.idToken);
+    console.log("accessToken: ", authResult.accessToken);
+    console.log("auth Result: ", authResult);
+    console.log("idTokenPayload: ", authResult.idTokenPayload);
+    console.log("LOGGING STUFF");
+    // Also Need a refresh token that doesnt change. currently it is null
+
+
+    ///// TEST the fitbit call
+    // console.log(process.env.REACT_APP_JeremyTOKEN); // not working
+    // console.log(process.env.REACT_APP_JeremyUSERID); // not working
+    // console.log(process.env.REACT_APP_JeremyUSERID);
+    let accesstoken = authResult.accessToken;
+    console.log('YOUR ACCESS TOKEN IS: ' + accesstoken)
 
     let config = {
       headers: {
-        'Authorization': "bearer " + authResult.idToken
+        Authorization: `Basic ${authResult.accessToken}`
       }
     };
-    axios.get(
-      'https://api.fitbit.com/1/user/7WTJLB/profile.json',
-      config
-    ).then((response) => {
+    let url = 'https://api.fitbit.com/1/user/7WTJLB/activities/date/2019-11-26.json';
+    // 'https://api.fitbit.com/1/user/7WX3P3/profile.json',
+    // 'https://api.fitbit.com/1/user/7WTJLB/activities/date/2019-11-26.json', Isabela's user id with activities
+    // 'https://api.fitbit.com/1/user/7WX3P3/activities/date/2019-11-26.json', // Jeremy's user id with activities
+    // config 
+    let request = url + { headers: { Authorization: `Basic ${authResult.accessToken}` } };
+
+    console.log('REQUEST: ' + request)
+
+
+    axios.get(url, config)
+    .then((response) => {
       console.log(response)
+      console.log(response.data);
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(response.headers);
+      console.log(response.config);
+      console.log("AUTH ATTEMPT!");
     }).catch((error) => {
       console.log(error)
+      console.log("IT BROKE");
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
     });
 
 
@@ -85,7 +128,7 @@ class Auth {
     this.profile = authResult.idTokenPayload;
     this.accessToken = authResult.accessToken;
     // set the time that the id token will expire at
-    this.expiresAt = authResult.idTokenPayload.exp * 1000;
+    this.expires_in = authResult.idTokenPayload.exp * 1000 * 60 * 10;
   }
 
 
