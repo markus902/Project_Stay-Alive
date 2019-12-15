083b8bb5cfc862e2a44f546ddbeebcba63b5cd2f
Markus Markowich <markus902@gmx.de>
2019-12-15 16:46:30 -0500

    added padding to logo

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ import { faBiohazard } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <FontAwesomeIcon icon={faBiohazard} style={{ color: '#B91D21' }} spin size='3x' />
-    <h5>
+    <h5 style={{ marginTop: '10px' }}>
       Stay Alive!© Developed by <a href="https://github.com/markus902/Stay_Alive">Critical Code</a>
     </h5>
   </footer>
